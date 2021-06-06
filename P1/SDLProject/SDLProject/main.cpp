842957854997488c679175c0f7d99857a2e8a0e5
Austen Ariniello <ama1105@nyu.edu>
2021-06-06 19:34:30 -0400

    Lecture 3 Code

--- P1/SDLProject/SDLProject/main.cpp
+++ P1/SDLProject/SDLProject/main.cpp
@@ -7,13 +7,48 @@
 #define GL_GLEXT_PROTOTYPES 1
 #include <SDL.h>
 #include <SDL_opengl.h>
+#include "glm/mat4x4.hpp"
+#include "glm/gtc/matrix_transform.hpp"
+#include "ShaderProgram.h"
+
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
 
 SDL_Window* displayWindow;
 bool gameIsRunning = true;
 
-int main(int argc, char* argv[]) {
+ShaderProgram program;
+glm::mat4 viewMatrix, modelMatrix, projectionMatrix;
+
+GLuint playerTextureID;
+GLuint LoadTexture(const char* filePath) {
+ int w, h, n;
+ unsigned char* image = stbi_load(filePath, &w, &h, &n, STBI_rgb_alpha);
+
+ if (image == NULL) {
+ std::cout << "Unable to load image. Make sure the path is correct\n";
+ assert(false);
+ }
+ 
+ GLuint textureID;
+ glGenTextures(1, &textureID);
+ glBindTexture(GL_TEXTURE_2D, textureID);
+ glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, w, h, 0, GL_RGBA, GL_UNSIGNED_BYTE, image);
+
+ glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+ glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+
+ stbi_image_free(image);
+ return textureID;
+}
+
+float player_x = 0;
+float lastTicks = 0.0f;
+float player_rotate = 0;
+
+void Initialize() {
     SDL_Init(SDL_INIT_VIDEO);
-    displayWindow = SDL_CreateWindow("Hello, World!", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 640, 480, SDL_WINDOW_OPENGL);
+    displayWindow = SDL_CreateWindow("Textured!", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 640, 480, SDL_WINDOW_OPENGL);
     SDL_GLContext context = SDL_GL_CreateContext(displayWindow);
     SDL_GL_MakeCurrent(displayWindow, context);
     
@@ -21,20 +56,101 @@ int main(int argc, char* argv[]) {
     glewInit();
 #endif
     
-    glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+    glViewport(0, 0, 640, 480);
     
+    program.Load("shaders/vertex_textured.glsl", "shaders/fragment_textured.glsl");
+    
+    viewMatrix = glm::mat4(1.0f);
+    modelMatrix = glm::mat4(1.0f);
+    projectionMatrix = glm::ortho(-5.0f, 5.0f, -3.75f, 3.75f, -1.0f, 1.0f);
+    
+    program.SetProjectionMatrix(projectionMatrix);
+    program.SetViewMatrix(viewMatrix);
+    // program.SetColor(1.0f, 0.0f, 0.0f, 1.0f);
+    
+    glUseProgram(program.programID);
+    
+    glClearColor(0.2f, 0.2f, 0.2f, 1.0f);
+    glEnable(GL_BLEND);
+    
+    // Good setting for transparency
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+    
+    playerTextureID = LoadTexture("ctg.png");
+}
+
+void ProcessInput() {
     SDL_Event event;
-    while (gameIsRunning) {
-        while (SDL_PollEvent(&event)) {
-            if (event.type == SDL_QUIT || event.type == SDL_WINDOWEVENT_CLOSE) {
-                gameIsRunning = false;
-            }
+    while (SDL_PollEvent(&event)) {
+        if (event.type == SDL_QUIT || event.type == SDL_WINDOWEVENT_CLOSE) {
+            gameIsRunning = false;
         }
-        
-        glClear(GL_COLOR_BUFFER_BIT);
-        SDL_GL_SwapWindow(displayWindow);
+    }
+}
+
+void Update() {
+    float ticks = (float)SDL_GetTicks() / 1000.0f;
+    float deltaTime = ticks - lastTicks;
+    lastTicks = ticks;
+    
+    
+    player_x += 1.0f * deltaTime;
+    player_rotate += 90.0f * deltaTime;
+    
+    modelMatrix = glm::mat4(1.0f);
+    modelMatrix = glm::translate((modelMatrix), glm::vec3(player_x, 0.0f, 0.0f));
+    modelMatrix = glm::rotate(modelMatrix, glm::radians(player_rotate), glm::vec3(0.0f, 0.0f, 1.0f));
+    
+    
+    // modelMatrix = glm::mat4(1.0f);
+    
+    /* // Moving Vertically Up
+    modelMatrix = glm::translate(modelMatrix, glm::vec3(0.0f, 0.01f, 0.0f));
+     */
+    
+    /* // Rotating
+    modelMatrix = glm::rotate(modelMatrix, glm::radians(1.0f), glm::vec3(0.0f, 0.0f, 1.0f));
+     */
+    
+    /* // Scaling
+    modelMatrix = glm::scale(modelMatrix, glm::vec3(1.01f, 1.01f, 1.0f));
+     */
+}
+
+void Render() {
+    glClear(GL_COLOR_BUFFER_BIT);
+    program.SetModelMatrix(modelMatrix);
+    
+    float vertices[] = { -0.5, -0.5, 0.5, -0.5, 0.5, 0.5, -0.5, -0.5, 0.5, 0.5, -0.5, 0.5 };
+    float texCoords[] = { 0.0, 1.0, 1.0, 1.0, 1.0, 0.0, 0.0, 1.0, 1.0, 0.0, 0.0, 0.0 };
+    
+    glVertexAttribPointer(program.positionAttribute, 2, GL_FLOAT, false, 0, vertices);
+    glEnableVertexAttribArray(program.positionAttribute);
+    glVertexAttribPointer(program.texCoordAttribute, 2, GL_FLOAT, false, 0, texCoords);
+    glEnableVertexAttribArray(program.texCoordAttribute);
+    
+    glBindTexture(GL_TEXTURE_2D, playerTextureID);
+    glDrawArrays(GL_TRIANGLES, 0, 6);
+    
+    glDisableVertexAttribArray(program.positionAttribute);
+    glDisableVertexAttribArray(program.texCoordAttribute);
+
+    SDL_GL_SwapWindow(displayWindow);
+}
+
+void Shutdown() {
+    SDL_Quit();
+}
+
+int main(int argc, char* argv[]) {
+    Initialize();
+    
+    while (gameIsRunning) {
+        ProcessInput();
+        Update();
+        Render();
     }
     
-    SDL_Quit();
+    Shutdown();
     return 0;
 }
