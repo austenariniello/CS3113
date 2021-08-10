9ed693d3b2193e5e1ccb16e31901784df20d7f21
Austen Ariniello <ama1105@nyu.edu>
2021-08-10 15:49:01 -0400

    Project 6 Level 2

--- P6/FInal Project/SDLProject/Level3.cpp
+++ P6/FInal Project/SDLProject/Level3.cpp
@@ -1,7 +1,7 @@
 #include "Level3.h"
 
-#define LEVEL3_WIDTH 30
-#define LEVEL3_HEIGHT 8
+#define LEVEL3_WIDTH 10
+#define LEVEL3_HEIGHT 50
 
 #define LEVEL3_ENEMY_COUNT 1
 
@@ -9,14 +9,56 @@
 
 unsigned int level3_data[] =
 {
-    38, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16,
-    50, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16,
-    50, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16,
-    50, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16,
-    50, 16, 16, 16, 16, 16, 16, 16, 16, 54, 55, 55, 56, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 42, 43, 44,
-    50, 16, 16, 16, 16, 54, 56, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 54, 56, 16, 16, 16, 16, 16, 16, 16, 16,
-    50, 16, 54, 56, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 54, 56, 16, 16, 16, 16, 54, 56, 16, 16, 16, 16, 16,
-    50, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16,
+    56, 56, 56, 56, 56, 56, 56, 56, 56, 56,
+    56, 56, 56, 56, 56, 56, 56, 56, 56, 56,
+    56, 56, 56, 56, 56, 56, 56, 56, 56, 56,
+    56, 56, 56, 56, 56, 56, 56, 56, 56, 56,
+    56, 56, 56, 56, 56, 56, 56, 56, 56, 56,
+    56, 56, 56, 56, 56, 56, 56, 56, 56, 56,
+    56, 56, 56, 56, 56, 56, 56, 56, 56, 56,
+    56, 56, 56, 56, 56, 56, 56, 56, 56, 56,
+    56, 56, 56, 56, 56, 56, 56, 56, 56, 56,
+    56, 56, 56, 56, 56, 56, 56, 56, 56, 56,
+    56, 56, 56, 56, 56, 56, 56, 56, 56, 56,
+    56, 56, 56, 56, 56, 56, 56, 56, 56, 56,
+    56, 56, 56, 56, 56, 56, 56, 56, 56, 56,
+    56, 56, 56, 56, 56, 56, 56, 56, 56, 56,
+    56, 56, 56, 56, 56, 56, 56, 56, 56, 56,
+    56, 56, 56, 56, 56, 56, 56, 56, 56, 56,
+    56, 56, 56, 56, 56, 56, 56, 56, 56, 56,
+    56, 56, 56, 56, 56, 56, 56, 56, 56, 56,
+    56, 56, 56, 56, 56, 56, 56, 56, 56, 56,
+    56, 56, 56, 56, 56, 56, 56, 56, 56, 56,
+    56, 56, 56, 56, 56, 56, 56, 56, 56, 56,
+    56, 56, 56, 56, 56, 56, 56, 56, 56, 56,
+    56, 56, 56, 56, 56, 56, 56, 56, 56, 56,
+    56, 56, 56, 56, 56, 56, 56, 56, 56, 56,
+    56, 56, 56, 56, 56, 56, 56, 56, 56, 56,
+    56, 56, 56, 56, 56, 56, 56, 56, 56, 56,
+    56, 56, 56, 56, 56, 56, 56, 56, 56, 56,
+    56, 56, 56, 56, 56, 56, 56, 56, 56, 56,
+    56, 56, 56, 56, 56, 56, 56, 56, 56, 56,
+    56, 56, 56, 56, 56, 56, 56, 56, 56, 56,
+    56, 56, 56, 56, 56, 56, 56, 56, 56, 56,
+    56, 56, 56, 56, 56, 56, 56, 56, 56, 56,
+    56, 56, 56, 56, 56, 56, 56, 56, 56, 56,
+    56, 56, 56, 56, 56, 56, 56, 56, 56, 56,
+    56, 56, 56, 56, 56, 56, 56, 56, 56, 56,
+    56, 56, 56, 56, 56, 56, 56, 56, 56, 56,
+    56, 56, 56, 56, 56, 56, 56, 56, 56, 56,
+    56, 56, 56, 56, 56, 56, 56, 56, 56, 56,
+    56, 56, 56, 56, 56, 56, 56, 56, 56, 56,
+    56, 56, 56, 56, 56, 56, 56, 56, 56, 56,
+    56, 56, 56, 56, 56, 56, 56, 56, 56, 56,
+    56, 56, 56, 56, 56, 56, 56, 56, 56, 56,
+    56, 56, 56, 56, 56, 56, 56, 56, 56, 56,
+    56, 56, 56, 56, 56, 56, 56, 56, 56, 56,
+    56, 56, 56, 56, 56, 56, 56, 56, 56, 56,
+    56, 56, 56, 56, 56, 56, 56, 56, 56, 56,
+    56, 56, 56, 56, 56, 56, 56, 56, 56, 56,
+    56, 56, 56, 56, 56, 56, 56, 56, 56, 56,
+    56, 56, 56, 56, 56, 56, 56, 56, 56, 56,
+    56, 56, 56, 56, 56, 56, 56, 56, 56, 56
 };
 
 void Level3::Initialize(int playerLives) {
@@ -32,25 +74,16 @@ void Level3::Initialize(int playerLives) {
     // Initialize Player
     state.player = new Entity();
     state.player->entityType = PLAYER;
-    state.player->position = glm::vec3(2.5, 0, 0);
-    state.player->startPosition = glm::vec3(2.5, 0, 0);
+    state.player->position = glm::vec3(4.5f, -47, 0);
+    state.player->startPosition = glm::vec3(4.5f, -47, 0);
+    state.player->velocity = glm::vec3(0, 1, 0);
     state.player->movement = glm::vec3(0);
-    state.player->acceleration = glm::vec3(0, -9.81f, 0);
-    state.player->speed = 2.7f;
+    state.player->acceleration = glm::vec3(0);
+    state.player->speed = 2.5f;
     state.player->textureID = Util::LoadTexture("ship_0000.png");
     
-    state.player->animRight = new int[6] {0, 1, 2, 3, 4, 5};
-    state.player->animLeft = new int[6] {6, 7, 8, 9, 10, 11};
-
-    state.player->animIndices = state.player->animRight;
-    state.player->animFrames = 4;
-    state.player->animIndex = 0;
-    state.player->animTime = 0;
-    state.player->animCols = 6;
-    state.player->animRows = 2;
-    
     state.player->height = 1;
-    state.player->width = 0.8f;
+    state.player->width = 1.0f;
     
     state.player->playerLives = playerLives;
     
@@ -60,8 +93,7 @@ void Level3::Initialize(int playerLives) {
     for (int i = 0; i < LEVEL3_ENEMY_COUNT; i++) {
         state.enemies[i].entityType = ENEMY;
         state.enemies[i].textureID = enemyTextureID;
-        state.enemies[i].position = glm::vec3(12 + i, -3, 0);
-        state.enemies[i].acceleration = glm::vec3(0, -9.81f, 0);
+        state.enemies[i].acceleration = glm::vec3(0);
         
         state.enemies[i].aiType = HOVER;
         state.enemies[i].aiState = IDLE;
@@ -86,10 +118,7 @@ void Level3::Initialize(int playerLives) {
 void Level3::Update(float deltaTime) {
     state.player->Update(deltaTime, state.player, state.enemies, LEVEL3_ENEMY_COUNT, state.projectiles, LEVEL3_PROJECTILE_COUNT, state.map);
     
-    if (state.player->position.y <= -10) {
-        state.player->PlayerHit();
-    }
-    else if (state.player->position.x >= 28) {
+    if ((state.player->position.y >= -4)) {
         state.nextScene = 4;
     }
     
